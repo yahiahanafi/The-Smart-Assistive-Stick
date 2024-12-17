89fcfba88fbc38207418b6f136948dbf6801a4bf
Yahia Hanafi <161923464+yahiahanafi@users.noreply.github.com>
2024-12-17 18:08:15 +0200

    Delete WebsiteGPS directory

--- WebsiteGPS/backend/routes/GPSRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const  {addGPS, getGPS, editGPS } = require('../controllers/GPSController');
-const router = express.Router();
-
-router.post('/', addGPS);
-router.get('/', getGPS);
-router.put('/', editGPS);
-
-module.exports = router;
